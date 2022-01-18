d9ac714dd379b258c1856f7a258ff7aa01e0290d
Santi <santi.cardozosartori@gmail.com>
2022-01-18 14:21:19 +0100

    Added option to iterate for x weeks

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	// Teatre | Parc Central |  Pista    | Plaça | Passarel·la
 	// Aneto  |  Pedraforca	 | Matagalls | Cadí  | Puigmal
+
+	// Initialize places
 	passarela := &models.Place{Name: "Pasarel·la", Next: nil}
 	plaza := &models.Place{Name: "Plaça", Next: passarela}
 	pista := &models.Place{Name: "Pista", Next: plaza}
@@ -16,6 +18,7 @@ func main() {
 	teatre := &models.Place{Name: "Teatre", Next: parcCentral}
 	passarela.Next = teatre
 
+	// Start groups and assign places
 	ant := &models.Group{Name: "Aneto", Place: teatre}
 	pdf := &models.Group{Name: "Pedraforca", Place: parcCentral}
 	mtg := &models.Group{Name: "Matagalls", Place: pista}
@@ -23,9 +26,18 @@ func main() {
 	pgm := &models.Group{Name: "Puigmal", Place: passarela}
 
 	groups := models.GroupsList{GroupsList: []*models.Group{ant, pdf, mtg, cdi, pgm}}
+
+	/**
+	ShowGroupsPlaces(groups) // Shows current config
+	groups.NextIteration()   // Switches to next iteration
+	ShowGroupsPlaces(groups) // Shows next iteration
+	**/
+
+	fmt.Println("Initial state:")
 	ShowGroupsPlaces(groups)
-	groups.NextIteration()
-	ShowGroupsPlaces(groups) //next iteration
+	fmt.Println("Iterations:")
+	IterateNextWeeks(20, groups)
+	// ShowGroupsPlaces(newGroups)
 
 }
 
@@ -34,4 +46,15 @@ func ShowGroupsPlaces(groups models.GroupsList) {
 		fmt.Printf("%v - %v\n", group.Name, group.Place.Name)
 	}
 	fmt.Println("--------------------------")
+}
+
+func IterateNextWeeks(weeks int, groups models.GroupsList) models.GroupsList {
+	for i := 0; i < weeks; i++ {
+		groups.NextIteration()
+
+		// For debugging. TODO: Comment out
+		fmt.Printf("Iteration number %02d\n", i+1)
+		ShowGroupsPlaces(groups)
+	}
+	return groups
 }
