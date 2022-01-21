f53c4afe0c1e3b7ef0a482b651f758726d4d8f22
Santi <santi.cardozosartori@gmail.com>
2022-01-22 00:17:13 +0100

    Algorithm now takes into account nonWeeks

--- main.go
+++ main.go
@@ -18,6 +18,14 @@ const (
 	startDay   = 17
 )
 
+// TODO: Delegate the storage and retrieval of nonWeeks to another gRPC endpoint
+// Day must always be a saturday
+var nonWeeks = []time.Time{
+	(&models.DateTime{Year: 2022, Month: 1, Day: 22}).ToTime(),
+	(&models.DateTime{Year: 2022, Month: 2, Day: 5}).ToTime(),
+	(&models.DateTime{Year: 2022, Month: 3, Day: 19}).ToTime(),
+}
+
 type GroupsServer struct {
 	pb.UnimplementedGroupsServer
 }
@@ -26,13 +34,14 @@ func (s *GroupsServer) GetGroupPlaces(ctx context.Context, dateRequest *pb.DateR
 	date := dateRequest.Date
 	log.Printf("Got new request for %v-%v-%v\n", date.Year, date.Month, date.Day)
 	startDate := models.DateTime{Year: startYear, Month: startMonth, Day: startDay}
-	endDate := models.DateTime{Year: date.Year, Month: date.Month, Day: date.Day}
+	requestedDate := models.DateTime{Year: date.Year, Month: date.Month, Day: date.Day}
 
-	weekNum := CalcWeekNum(startDate.ToTime(), endDate.ToTime())
+	iterNum := CalcWeekNumNoWeeks(startDate.ToTime(), requestedDate.ToTime(), nonWeeks)
 	groups := GetInitialState()
 
-	futureGroups := IterateNextWeeks(weekNum, groups)
+	futureGroups := IterateNextWeeks(iterNum, groups)
 	futureGroupsApiModel := LocalGroupModelToApi(futureGroups, date)
+
 	return &pb.GroupsPlacesResponse{
 		Groups: futureGroupsApiModel,
 	}, nil
@@ -61,7 +70,6 @@ func main() {
 //   <-          <-           <-        <-         <-
 // Aneto  |  Pedraforca	 | Matagalls | Cadí  | Puigmal
 func GetInitialState() models.GroupsList {
-
 	// Initialize places
 	passarela := &models.Place{Name: "Pasarel·la", Next: nil}
 	plaza := &models.Place{Name: "Plaça", Next: passarela}
@@ -70,15 +78,6 @@ func GetInitialState() models.GroupsList {
 	teatre := &models.Place{Name: "Teatre", Next: parcCentral}
 	passarela.Next = teatre
 
-	/**
-	ant := &models.Group{Name: "Aneto", Place: teatre}
-	pdf := &models.Group{Name: "Pedraforca", Place: parcCentral}
-	mtg := &models.Group{Name: "Matagalls", Place: pista}
-	cdi := &models.Group{Name: "Cadí", Place: plaza}
-	pgm := &models.Group{Name: "Puigmal", Place: passarela}
-	groups := models.GroupsList{GroupsList: []*models.Group{ant, pdf, mtg, cdi, pgm}}
-	return groups
-	**/
 	// Start groups and assign places
 	return models.GroupsList{
 		GroupsList: []*models.Group{
@@ -120,7 +119,7 @@ func IterateNextWeeks(weeks int, groups models.GroupsList) models.GroupsList {
 	for i := 0; i < weeks; i++ {
 		groups.NextIteration()
 
-		// For debugging
+		// For debugging purposes
 		/**
 		fmt.Printf("Iteration number %02d\n", i+1)
 		ShowGroupsPlaces(groups)
@@ -129,13 +128,36 @@ func IterateNextWeeks(weeks int, groups models.GroupsList) models.GroupsList {
 	return groups
 }
 
-// Calculates number of weeks for which to know their respective state in the future
+// Deprecated: Calculates number of weeks for which to know their respective state in the future
 func CalcWeekNum(startDateTime time.Time, endDateTime time.Time) int {
-
-	//t2 := time.Now().UTC()
-	//t2 := time.Date(2022, time.Month(1), 31, 1, 0, 0, 0, time.UTC)
 	days := endDateTime.Sub(startDateTime).Hours() / 24
 	weeks := int(days / 7)
 
 	return weeks
+}
+
+// Calculates number of weeks for which to know their respective state in the future
+// Takes into account startDate, the requested date and the list of days in which state won't change
+func CalcWeekNumNoWeeks(startDate time.Time, requestedDate time.Time, nonWeeks []time.Time) int {
+	// Convert any non-Monday date to Monday
+	// This preserves state for the entire week, as if it were Monday
+	if reqWeekDay := int(requestedDate.Weekday()); reqWeekDay != 5 {
+		sub := reqWeekDay
+		if reqWeekDay == 0 {
+			sub = 7
+		}
+		requestedDate = requestedDate.AddDate(0, 0, 5-sub)
+	}
+	// Calculate number of weeks since startDate
+	days := requestedDate.Sub(startDate).Hours() / 24
+	weeks := int(days / 7)
+
+	// Compute total number of nonWeek occurrences since startDate, that happen before the requestedDate
+	var sub int
+	for _, time := range nonWeeks {
+		if time.After(startDate) && time.Before(requestedDate) {
+			sub += 1
+		}
+	}
+	return weeks - sub
 }
